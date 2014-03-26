1c6018659579607437f47e4cb184bca8e2f0b9d7
lobsterhands <denmanlp@gmail.com>
2014-03-25 23:30:35 -0500

    Initial commit

---
+++ script/buttoncolor.js
@@ -0,0 +1,10 @@
+function mOver(obj) {
+  obj.style.backgroundColor="#444444"
+  obj.style.color="white"
+  obj.innerHTML="Learn what I do"
+}
+function mOut(obj) {
+  obj.style.backgroundColor="#312730"
+  obj.style.color="white"
+  obj.innerHTML="Learn what I do"
+}
